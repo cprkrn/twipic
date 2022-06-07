918ca3c041e75b9bd2702e52d78f5cbaf4ab7d6b
Soup <9752976+cprkrn@users.noreply.github.com>
2022-06-07 18:04:18 -0400

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-api_key = "oRqLhk0My3tDLt1o1DNUleFwo"
-api_secret = "xgGtoTEBiGDrJlmxEyZTiUNLSKqA5EJLnSPPXQqybbbW3nT8Wm"
-consumer_key = "734048454-uSRSCHpv993OiAbTbdLVYYIzPIB7ARMe6zW3rkne"
-secret_key = "7X2vC7GDze8VWAAyU7t7RlqwV2aNXRzp9vqA0pMNYGDIq"
